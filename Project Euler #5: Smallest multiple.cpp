df49c49e75c140041e2aa6ebdb3215c3b0f48ee2
vikas2cc <vikasiitianindia@gmail.com>
2019-07-25 14:05:36 +0530

    Create Project Euler #5: Smallest multiple.cpp

---
+++ Project Euler #5: Smallest multiple.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int ans[40]={0};
+
+void precalculate(){
+    for(int i=0; i<40; i++){
+        ans[i]=i+1;
+        if(i!=0){
+            ans[i]=(ans[i]*ans[i-1])/__gcd(ans[i],ans[i-1]);
+        }
+    }
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    precalculate();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<ans[n-1]<<endl;
+    }
+    return 0;
+}
